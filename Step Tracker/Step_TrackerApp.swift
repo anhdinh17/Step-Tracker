3c554fe5e07d85fbe7a4b5579296923413ec1ba4
Anh Dinh <anhdinh17@gmail.com>
2024-06-24 17:31:43 +0700

    Create XCode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Anh Dinh on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
